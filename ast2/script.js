d916eea7f3fa50a5a4301f9af6e51ae9144bf4d0
Amit Maharjan <maharjanamit8@gmail.com>
2019-04-23 21:52:37 +0545

    Assignment 2 Completed

--- ast2/script.js
+++ ast2/script.js
@@ -7,6 +7,7 @@ const xMin = 0 + radius;
 const xMax = window.innerWidth - 25 - radius;
 const yMin = 0 + radius;
 const yMax = window.innerHeight - 25 - radius;
+const velocity = 3;
 
 let canvas = document.getElementById('canvas');
 
@@ -49,12 +50,12 @@ function Circle(x, y, radius, color, dx, dy) {
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  var n = Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  let num = Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 
-  if (n === 0) {
+  if (num === 0) {
     getRandomInt(min, max);
   } else {
-    return n;
+    return num;
   }
 }
 
@@ -68,8 +69,28 @@ for (let i = 0; i < numberOfBalls; i++) {
   let gValue = getRandomInt(0, 255);
   let bValue = getRandomInt(0, 255);
   let color = 'rgb(' + rValue + ',' + gValue + ',' + bValue + ')';
-  let dx = getRandomInt(-3, 3); //-3 and 3 are just the random velocity
-  let dy = getRandomInt(-3, 3);
+  let dx = getRandomInt(-velocity, velocity);
+  let dy = getRandomInt(-velocity, velocity);
+
+  //This was the code for checking overlapping coordinates before drawing circles but it made broswer slow for circles < 500
+  /*
+  if (i !== 0) {
+    for (let j = 0; j < circles.length; j++) {
+      let xDistance = x - circles[j].x;
+      let yDistance = y - circles[j].y;
+      let distanceBetweenThem = Math.sqrt(
+        xDistance * xDistance + yDistance * yDistance
+      );
+
+      if(distanceBetweenThem === radius * 2){
+        let x = getRandomInt(xMin, xMax);
+        let y = getRandomInt(yMin, yMax);
+
+        j=-1;//For repeating the loop again and again until non-overlapping circles are generated
+      }
+    }
+  }
+  */
 
   circles[i] = new Circle(x, y, radius, color, dx, dy);
   circles[i].drawCircle();
@@ -82,15 +103,24 @@ function animate() {
 
   for (let i = 0; i < circles.length; i++) {
     circles[i].update();
-    //collide();
+    //checkCollisione();
   }
-  collide();
+  checkCollision();
 }
 
 animate();
 
+//Functions For Rotation
+function rotateVelocityX(dx, dy, angle) {
+  return dx * Math.cos(angle) - dy * Math.sin(angle);
+}
+
+function rotateVelocityY(dx, dy, angle) {
+  return dx * Math.sin(angle) + dy * Math.cos(angle);
+}
+
 //Collision Detection
-function collide() {
+function checkCollision() {
   for (let i = 0; i < circles.length; i++) {
     for (let j = 0; j < circles.length; j++) {
       if (i != j) {
@@ -101,13 +131,58 @@ function collide() {
         );
 
         if (distance < circles[i].radius + circles[j].radius) {
-          // collision detected!
+          //This just reverses the velocities when collided, so a realistic approach was made below
+          /*
+          collision detected!
           circles[i].dx = -circles[i].dx;
           circles[i].dy = -circles[i].dy;
           circles[i].update();
 
           circles[j].dx = -circles[j].dx;
           circles[j].dy = -circles[j].dy;
+          circles[j].update();
+          */
+
+          /*The Math.atan2() function in JavaScript is used to return the arctangent of the quotient of its arguments.
+          The Math.atan2() method returns a numeric value between -Π and Π representing the angle theta of an (x, y) point
+          and positive x axis. This is the counterclockwise angle, measured in radians, between the positive X axis, and the point (x, y).*/
+          let angle = -Math.atan2(
+            circles[i].y - circles[j].y,
+            circles[i].x - circles[j].x
+          );
+
+          let m1 = 1; //Mass of circle 1
+          let m2 = 1; //Mass of circle 2
+
+          // Velocity before equation
+          let u1X = rotateVelocityX(circles[i].dx, circles[i].dy, angle);
+          let u1y = rotateVelocityY(circles[i].dx, circles[i].dy, angle);
+
+          let u2X = rotateVelocityX(circles[j].dx, circles[j].dy, angle);
+          let u2Y = rotateVelocityY(circles[j].dx, circles[j].dy, angle);
+
+          // Velocity after 1d collision newtonian equation
+          let v1X = (u1X * (m1 - m2)) / (m1 + m2) + (u2X * 2 * m2) / (m1 + m2);
+          let v1Y = u1y;
+
+          let v2X = (u2X * (m1 - m2)) / (m1 + m2) + (u1X * 2 * m2) / (m1 + m2);
+          let v2Y = u2Y;
+
+          // Final velocity after rotating axis back to original location
+          let vFinal1X = rotateVelocityX(v1X, v1Y, -angle);
+          let vFinal1Y = rotateVelocityY(v1X, v1Y, -angle);
+
+          let vFinal2X = rotateVelocityX(v2X, v2Y, -angle);
+          let vFinal2Y = rotateVelocityY(v2X, v2Y, -angle);
+
+          // Swap circles velocities for realistic bounce effect
+          circles[i].dx = vFinal1X;
+          circles[i].dy = vFinal1Y;
+
+          circles[j].dx = vFinal2X;
+          circles[j].dy = vFinal2Y;
+
+          circles[i].update();
           circles[j].update();
         }
       }
